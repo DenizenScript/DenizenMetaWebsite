3ccbc2f82dd176029480a4434aede54ab47aeb27
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-03-31 17:53:00 -0700

    probably less bad meta event name lines

--- MetaObjects/WebsiteMetaAction.cs
+++ MetaObjects/WebsiteMetaAction.cs
@@ -12,8 +12,16 @@ namespace DenizenMetaWebsite.MetaObjects
         {
             HtmlContent = HTML_PREFIX;
             string aID = Util.EscapeForHTML(Object.CleanName);
-            string fullNameText = string.Join("\n<br>", Object.MultiNames.Select(n => Util.EscapeForHTML(n)));
-            HtmlContent += TableLine("primary", "Name", $"<a id=\"{aID}\" href=\"#{aID}\" onclick=\"doFlashFor('{aID}')\"><span class=\"doc_name\">{fullNameText}</span></a>", false);
+            string fullNameText;
+            if (Object.HasMultipleNames)
+            {
+                fullNameText = $"<span class=\"doc_name\">{Util.EscapeForHTML(Object.Name)}</span>\n<br>" + string.Join("\n<br>", Object.MultiNames.Skip(1).Select(s => Util.EscapeForHTML(s)));
+            }
+            else
+            {
+                fullNameText = $"<span class=\"doc_name\">{Util.EscapeForHTML(Object.Name)}</span>";
+            }
+            HtmlContent += TableLine("primary", "Action Lines", $"<a id=\"{aID}\" href=\"#{aID}\" onclick=\"doFlashFor('{aID}')\">{fullNameText}</a>", false);
             HtmlContent += TableLine("active", "Triggers", Object.Triggers, true);
             HtmlContent += TableLine("active", "Contexts", WebsiteMetaCommand.HtmlizeTags(Object.Context, Object.Meta), false);
             HtmlContent += TableLine("active", "Determine", string.Join("\n", Object.Determinations), true);
