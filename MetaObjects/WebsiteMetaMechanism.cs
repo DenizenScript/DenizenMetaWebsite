6db5e04e03176999fd7556f44e79418004bec54e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-07-03 08:24:39 -0700

    better mechanism format

--- MetaObjects/WebsiteMetaMechanism.cs
+++ MetaObjects/WebsiteMetaMechanism.cs
@@ -12,7 +12,8 @@ namespace DenizenMetaWebsite.MetaObjects
         {
             HtmlContent = HTML_PREFIX;
             string aID = Util.EscapeForHTML(Object.CleanName);
-            HtmlContent += TableLine("primary", "Name", $"<a id=\"{aID}\" href=\"#{aID}\" onclick=\"doFlashFor('{aID}')\"><span class=\"doc_name\">{Util.EscapeForHTML(Object.FullName)}</span></a>", false);
+            HtmlContent += TableLine("primary", "Name", $"<a id=\"{aID}\" href=\"#{aID}\" onclick=\"doFlashFor('{aID}')\"><span class=\"doc_name\">{Util.EscapeForHTML(Object.MechName)}</span></a>", false);
+            HtmlContent += TableLine("active", "Object", $"<a href=\"/ObjectTypes/{Util.EscapeForHTML(Object.MechObject)}\">{Util.EscapeForHTML(Object.MechObject)}</a>", false);
             HtmlContent += TableLine("active", "Input", Object.Input, true);
             HtmlContent += TableLine("active", "Related Tags", WebsiteMetaCommand.HtmlizeTags(Object.Tags, Object.Meta), false);
             HtmlContent += TableLine("active", "Description", Object.Description, true);
