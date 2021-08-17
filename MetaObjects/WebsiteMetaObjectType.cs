f12385b1f0ca432b14dea041d2ecce404bf6c706
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-08-16 17:03:52 -0700

    improve meta object types handling code

--- MetaObjects/WebsiteMetaObjectType.cs
+++ MetaObjects/WebsiteMetaObjectType.cs
@@ -9,18 +9,19 @@ namespace DenizenMetaWebsite.MetaObjects
 {
     public class WebsiteMetaObjectType : WebsiteMetaObject<MetaObjectType>
     {
+        public string LinkObjectType(MetaObjectType type) => $"<a href=\"/Docs/ObjectTypes/{Util.EscapeForHTML(type.CleanName)}\">{Util.EscapeForHTML(type.Name)}</a>";
+
         public override void LoadHTML()
         {
             HtmlContent = HTML_PREFIX;
             string aID = Util.EscapeForHTML(Object.CleanName);
             HtmlContent += TableLine("primary", "Name", $"<a id=\"{aID}\" href=\"#{aID}\" onclick=\"doFlashFor('{aID}')\"><span class=\"doc_name\">{Util.EscapeForHTML(Object.Name)}</span></a>", false);
             HtmlContent += TableLine("active", "Prefix", Object.Prefix.ToLowerFast() == "none" ? "None" : $"{Object.Prefix}@", true);
-            HtmlContent += TableLine("active", "Base Type", Object.BaseTypeName, true);
-            HtmlContent += TableLine("active", "Implements", string.Join(", ", Object.ImplementsNames.Select(n => $"<a href=\"/Docs/ObjectTypes/{Util.EscapeForHTML(n)}\">{Util.EscapeForHTML(n)}</a>")), false);
+            HtmlContent += TableLine("active", "Base Type", LinkObjectType(Object.BaseType), true);
+            HtmlContent += TableLine("active", "Implements", string.Join(", ", Object.Implements.Select(LinkObjectType)), false);
             HtmlContent += TableLine("active", "Identity Format", Object.Format, true);
             HtmlContent += TableLine("active", "Description", Object.Description, true);
-            IEnumerable<MetaObjectType> extendingTypes = Docs.ObjectTypes.Values.Where(obj => obj.BaseType == Object || obj.Implements.Contains(Object));
-            HtmlContent += TableLine("active", "Extended By", string.Join(", ", extendingTypes.Select(t => $"<a href=\"/Docs/ObjectTypes/{Util.EscapeForHTML(t.CleanName)}\">{Util.EscapeForHTML(t.Name)}</a>")), false);
+            HtmlContent += TableLine("active", "Extended By", string.Join(", ", Object.ExtendedBy.Select(LinkObjectType)), false);
             AddHtmlEndParts();
         }
 
