e0d82c129ccd73a28ef649fba1f22a1f844994e2
apademide <aurian.aubert@bluewin.ch>
2022-09-21 17:48:12 +0200

    Use localhost for testing

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dotnet restore
 dotnet build
-ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=http://*:8098 dotnet bin/Debug/net6.0/DenizenMetaWebsite.dll
+ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=http://localhost:8098 dotnet bin/Debug/net6.0/DenizenMetaWebsite.dll
